c9e4942093aa9fa596b58c1310cbff6e98d523c9
Yaron David Esquivel Ventura <Urlof_desty91@hotmail.com>
2022-04-28 23:04:56 -0500

    adding elements

---
+++ src/components/index2.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function index2() {
+    return (
+        <>
+    <h1>Trabaja aqui y si necesitas otro archivo usa este como plantilla</h1>
+        </>        
+    );
+}
