c4fea33886d3c755143a20f1f812e5a707613fd6
Yaron David Esquivel Ventura <Urlof_desty91@hotmail.com>
2022-04-28 23:07:55 -0500

    q

--- src/components/index2.js
+++ src/components/index2.js
@@ -4,6 +4,12 @@ export default function index2() {
     return (
         <>
     <h1>Trabaja aqui y si necesitas otro archivo usa este como plantilla</h1>
+    <h2>No copies y pegues todo el html, porque va a dar muuuuuuchos errores</h2><br/>
+    Los codigos de bootstrap van a tener algunos errores tambien, por ejemplo
+    los "class" debes cambiarlo por "className"
+    todas las etiquetas deben tener cierre, en algunas etiquetas HTML, no es decesario
+    Aqui es obligatorio
+    Para que lo hagas por partes
         </>        
     );
 }
