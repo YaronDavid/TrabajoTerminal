2b306e20067d46120a36d0d3284d8da31e56fb02
Yaron David Esquivel Ventura <Urlof_desty91@hotmail.com>
2022-11-16 22:43:53 -0600

    Sesiones
    Se cambio el navbar para que aparezcan ciertas opciones dependiendo de si se inicio sesion o no

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,71 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Logotipo from '../images/Logotipo.png'
 import fb from '../images/contacto-header/facebook.png'
 import tw from '../images/contacto-header/twitter.png'
 import insta from '../images/contacto-header/instagram.png'
 import './styles/Index2.css'
+import UserServices from '../services/UserServices';
 
 
 export default function NavBar() {
+
+  let log='';
+  let cerrar='';
+  let grupos='';
+
+  const[id,setId] = useState(null);
+  const[tipo,setTipo] = useState(null);
+
+  useEffect(()=>{
+    setId(sessionStorage.getItem('id'));
+    if(id){
+    UserServices.getUserById(id).then((res)=>{
+      setTipo(res.data.tipo_usuario);
+    }
+  
+    );
+  }
+    console.log(tipo);
+  });
+
+  const closeSesion = () =>{
+    sessionStorage.clear();
+  }
+  
+
+  if(!id){
+    log = <>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="registro">Registrarse</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/login">Iniciar sesion</a>
+            </li>
+          </>
+    cerrar = <></>
+    grupos = <></>
+  }else{
+    log = <></>
+    grupos =  <li className="nav-item">
+                <a className="nav-link active btn btn-close-session" aria-current="page" href="/myGroups">Mis grupos</a>
+              </li>
+    cerrar =  <li className="nav-item">
+                <a className="btn btn-close-session" aria-current="page" href="/" onClick={closeSesion}>Cerrar sesion</a>
+              </li>
+  }
+
+  if(tipo==0){
+
+  }
   return (
+    
     <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
+      
         <div className="container">
           <div className="container-fluid">
 
-            <a className="navbar-brand" href="index.html">
               <img className="logotipo" src={Logotipo} alt="" />
-            </a>
-
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -27,38 +73,8 @@ export default function NavBar() {
                 <li className="nav-item">
                   <a className="nav-link active" aria-current="page" href="/">Inicio</a>
                 </li>
-
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/exercises">Ejercicios</a>
-                </li>
-
-                <li className="nav-item dropdown">
-                  <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-
-                  </a>
-                  <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <li><a className="dropdown-item" href="#">Twitter</a></li>
-                    <li><a className="dropdown-item" href="#">Facebook</a></li>
-                    <li><hr className="dropdown-divider" /></li>
-                    <li><a className="dropdown-item" href="#">Something else here</a></li>
-                  </ul>
-                </li>
-
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="simulador.html">Simulador</a>
-                </li>
-
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="temas.html">Temas</a>
-                </li>
-
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="registroUsuario.html">Registrarse</a>
-                </li>
-
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/login">Iniciar sesion</a>
-                </li>
+                
+                {log}
 
                 <li>
                   <a className="nav-link active" aria-current="page">Contacto</a>
@@ -68,6 +84,7 @@ export default function NavBar() {
                   <a href="#"><img className="iconos-redes-header" src={tw} alt="Twitter" /></a>
                   <a href="#"><img className="iconos-redes-header" src={insta} alt="Instagram" /></a>
                 </li>
+                {cerrar}
               </ul>
 
             </div>
