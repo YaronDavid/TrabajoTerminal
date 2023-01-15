d37f6571da370c551b9f91c6a704e4cbc5cc21e3
Yaron David Esquivel Ventura <Urlof_desty91@hotmail.com>
2023-01-14 22:02:06 -0600

    Act Registro

--- src/components/Registro.js
+++ src/components/Registro.js
@@ -32,20 +32,32 @@ export default class Registro extends Component {
   
   Registro = (event) =>{
     event.preventDefault();
-    if(this.state.Password.length<5){
-      this.setState({err:<div className='red'>La contraseña debe tener minimo 5 caracteres</div>})
+    if(this.state.Name==''||this.state.Name==' '){
+      alert("Introduzca su nombre");
     }else{
-      if(this.state.Password==this.state.Password2){
-        let user = {tipo_usuario: this.state.Type, nombre: this.state.Name, apellido_paterno: this.state.LastName,
-        apellido_materno: this.state.MotherName, correo: this.state.Email, contrasena: this.state.Password, 
-        pregunta: this.state.SecurityQuestion, respuesta: this.state.SecurityAnswer }
-        this.setState({err:<></>});
-        console.log('usuario => ' + JSON.stringify(user));
-        UserServices.createUser(user).then(res =>{
-          window.location.href = '/';
-        });
+      if(this.state.SecurityQuestion==''){
+        alert("Seleccione una pregunta de seguridad");
       }else{
-        this.setState({err:<div className='red'>Contraseña incorrecta</div>});
+        if(this.state.Type==''){
+          alert("Seleccione un tipo de usuario");
+        }else{
+          if(this.state.Password.length<6){
+            alert("La contraseña es demasiado corta");
+          }else{
+            if(this.state.Password==this.state.Password2){
+              let user = {tipo_usuario: this.state.Type, nombre: this.state.Name, apellido_paterno: this.state.LastName,
+              apellido_materno: this.state.MotherName, correo: this.state.Email, contrasena: this.state.Password, 
+              pregunta: this.state.SecurityQuestion, respuesta: this.state.SecurityAnswer }
+              this.setState({err:<></>});
+              alert("Se registro el usuario correctamente, inicia sesión para acceder")
+              UserServices.createUser(user).then(res =>{
+                window.location.href = '/';
+              });
+            }else{
+              alert("Las contraseñas no coinciden");
+            }
+          }
+        }
       }
     }
   }
@@ -82,36 +94,36 @@ export default class Registro extends Component {
   
   render() {
     return (
-      <div className='body'>
+      <div className='body' id="create2">
       <br/>
       <div className='container'>
         <div className='row'>
           <div className='card col-md-6 offset-md-3 offset-md-3'>
             <h3 className='text-center tit'>Registrarse</h3>
             <div className='card-body'>
-              <form id="reg">
+              <form id="reg" onSubmit={this.Registro}>
                 <div className='form-group'>
-                <input placeholder='Nombre' name='nombre' className='form-control' onChange={this.changeHandleName}/><br/>
-                <input placeholder='Apellido paterno' name='apPat' className='form-control' onChange={this.changeHandleLastName}/><br/>
-                <input placeholder='Apellido materno' name='apMat' className='form-control' onChange={this.changeHandleMotherName}/><br/>
-                <input placeholder='Correo' name='correo' className='form-control' onChange={this.changeHandleEmail}/><br/>
+                <input placeholder='Nombre(s)' name='nombre' className='form-control' onChange={this.changeHandleName} required/><br/>
+                <input placeholder='Apellido paterno' name='apPat' className='form-control' onChange={this.changeHandleLastName} required/><br/>
+                <input placeholder='Apellido materno' name='apMat' className='form-control' onChange={this.changeHandleMotherName} required/><br/>
+                <input placeholder='Correo electrónico' type="email" name='correo' className='form-control' onChange={this.changeHandleEmail} required/><br/>
                 <select name="type" id="type" form="reg" onChange={this.changeHandleType}>
-                    <option value="" disabled selected>Seleccione tipo de usuario</option>
+                    <option value="" disabled selected>Seleccione su tipo de usuario</option>
                     <option value="1">Profesor</option>
                     <option value="0">Alumno</option>
                 </select><br/>
                 <br/>
-                <input placeholder='Constraseña' name='contrasena' className='form-control' minLength="6" onChange={this.changeHandlePassword}/><br/>
-                <input placeholder='Confirmar onstraseña' name='conf' className='form-control' onChange={this.changeHandlePassword2}/><br/>
+                <input placeholder='Constraseña (mínimo 6 caracteres)' name='contrasena' className='form-control' minLength="6" onChange={this.changeHandlePassword} required/><br/>
+                <input placeholder='Confirmar contraseña' name='conf' className='form-control' onChange={this.changeHandlePassword2} required/><br/>
                 <select name="QS" id="QS" form="reg" onChange={this.changeHandleSecurityQuestion}>
                     <option value="" disabled selected>Seleccione una pregunta de seguridad</option>
                     <option value="0">Nombre de tu primer mascota</option>
-                    <option value="1">Ultimo lugar que visitaste</option>
+                    <option value="1">Último lugar que visitaste</option>
                     <option value="2">¿Comó se llama tu mejor amigo?</option>
                 </select><br/>
                 <br/>
-                <input placeholder='Respuesta a la pregunta de seguridad' name='respuesta' className='form-control' onChange={this.changeHandleSecurityAnswer}/><br/>
-                <a className='btn' onClick={this.Registro}>Registrarse</a> <a className='btn' href='/'>Volver</a>
+                <input placeholder='Respuesta a la pregunta de seguridad' name='respuesta' className='form-control' onChange={this.changeHandleSecurityAnswer} required/><br/>
+                <input type="submit" className='btn' value="Registrarse"/><a className='btn' href='/'>Volver</a>
                 {this.state.err}
                 </div>
               </form>
